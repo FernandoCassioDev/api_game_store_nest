4afe56089b9fe18e3839a063b30c6d9a652da821
Fernando Cassio <fernandocassiodev@gmail.com>
2024-08-03 20:02:43 -0300

    feat: adiciona segurança nos endpoints de categorias e produtos

--- src/categoria/controllers/categoria.controller.ts
+++ src/categoria/controllers/categoria.controller.ts
@@ -1,45 +1,59 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
-import { CategoriaService } from "../services/categoria.service";
-import { Categoria } from "../entities/categoria.entity";
-import { DeleteResult } from "typeorm";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { CategoriaService } from '../services/categoria.service';
+import { Categoria } from '../entities/categoria.entity';
+import { DeleteResult } from 'typeorm';
+import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('/categorias')
 export class CategoriaController {
-    constructor(readonly categoriaService: CategoriaService) { }
+  constructor(readonly categoriaService: CategoriaService) {}
 
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Categoria[]> {
-        return this.categoriaService.findAll()
-    }
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaService.findAll();
+  }
 
-    @Get('/:id')
-    @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
-        return this.categoriaService.findById(id)
-    }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
+    return this.categoriaService.findById(id);
+  }
 
-    @Get('/tipo/:tipo')
-    @HttpCode(HttpStatus.OK)
-    findByType(@Param('tipo') tipo: string): Promise<Categoria[]> {
-        return this.categoriaService.findByType(tipo)
-    }
+  @Get('/tipo/:tipo')
+  @HttpCode(HttpStatus.OK)
+  findByType(@Param('tipo') tipo: string): Promise<Categoria[]> {
+    return this.categoriaService.findByType(tipo);
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() categoria: Categoria): Promise<Categoria> {
-        return this.categoriaService.create(categoria)
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.create(categoria);
+  }
 
-    @Put()
-    @HttpCode(HttpStatus.OK)
-    update(@Body() categoria: Categoria): Promise<Categoria>{
-        return this.categoriaService.update(categoria)
-    }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() categoria: Categoria): Promise<Categoria> {
+    return this.categoriaService.update(categoria);
+  }
 
-    @Delete('/:id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
-        return this.categoriaService.delete(id)
-    }
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.categoriaService.delete(id);
+  }
 }
--- src/produto/controllers/produto.controller.ts
+++ src/produto/controllers/produto.controller.ts
@@ -1,58 +1,71 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
-import { ProdutoService } from "../services/produto.service";
-import { Produto } from "../entities/produto.entity";
-import { DeleteResult } from "typeorm";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { ProdutoService } from '../services/produto.service';
+import { Produto } from '../entities/produto.entity';
+import { DeleteResult } from 'typeorm';
+import { JwtAuthGuard } from '../../auth/guard/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('/produtos')
 export class ProdutoController {
-    constructor(readonly produtoService: ProdutoService) { }
+  constructor(readonly produtoService: ProdutoService) {}
 
-    @Get()
-    @HttpCode(HttpStatus.OK)
-    findAll(): Promise<Produto[]> {
-        return this.produtoService.findAll()
-    };
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Produto[]> {
+    return this.produtoService.findAll();
+  }
 
-    @Get('/:id')
-    @HttpCode(HttpStatus.OK)
-    findById(@Param('id', ParseIntPipe) id: number): Promise<Produto>{
-        return this.produtoService.findById(id)
-    }
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Produto> {
+    return this.produtoService.findById(id);
+  }
 
-    @Get('/titulo/:titulo')
-    @HttpCode(HttpStatus.OK)
-    findByTitle(@Param('titulo') titulo: string): Promise<Produto[]>{
-        return this.produtoService.findByTitle(titulo)
-    }
+  @Get('/titulo/:titulo')
+  @HttpCode(HttpStatus.OK)
+  findByTitle(@Param('titulo') titulo: string): Promise<Produto[]> {
+    return this.produtoService.findByTitle(titulo);
+  }
 
-    @Post()
-    @HttpCode(HttpStatus.CREATED)
-    create(@Body() produto: Produto):Promise<Produto>{
-        return this.produtoService.create(produto)
-    }
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() produto: Produto): Promise<Produto> {
+    return this.produtoService.create(produto);
+  }
 
-    @Put()
-    @HttpCode(HttpStatus.OK)
-    update(@Body() produto: Produto): Promise<Produto>{
-        return this.produtoService.update(produto)
-    }
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() produto: Produto): Promise<Produto> {
+    return this.produtoService.update(produto);
+  }
 
-    @Delete(':id')
-    @HttpCode(HttpStatus.NO_CONTENT)
-    delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult>{
-        return this.produtoService.delete(id)
-    }
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+    return this.produtoService.delete(id);
+  }
 
-    @Get('/filtro/maior/:preco')
-    @HttpCode(HttpStatus.OK)
-    findByMore(@Param('preco', ParseIntPipe) preco: number): Promise<Produto[]>{
-        return this.produtoService.findByMore(preco)
-    }
+  @Get('/filtro/maior/:preco')
+  @HttpCode(HttpStatus.OK)
+  findByMore(@Param('preco', ParseIntPipe) preco: number): Promise<Produto[]> {
+    return this.produtoService.findByMore(preco);
+  }
 
-    @Get('/filtro/menor/:preco')
-    @HttpCode(HttpStatus.OK)
-    findByLess(@Param('preco', ParseIntPipe) preco: number){
-        return this.produtoService.findByLess(preco)
-    }
-
+  @Get('/filtro/menor/:preco')
+  @HttpCode(HttpStatus.OK)
+  findByLess(@Param('preco', ParseIntPipe) preco: number) {
+    return this.produtoService.findByLess(preco);
+  }
 }
